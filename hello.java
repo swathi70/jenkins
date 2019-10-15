b8f7b18cdb0c204ef575937e178c4fcaaea4e509
marella.swathi <31910732+swathi70@users.noreply.github.com>
2019-10-15 18:22:36 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,1 +1,6 @@
-System.out.println("hello world!")
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
+}
